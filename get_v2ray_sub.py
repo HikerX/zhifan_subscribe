4e15003e6ecaf0b64b0c2d347b54b6f75b97b6be
hiker <yinlaile@gmail.com>
2025-11-20 19:55:23 +0800

    add ss urls

--- get_v2ray_sub.py
+++ get_v2ray_sub.py
@@ -2,7 +2,9 @@ import requests;
 import re;
 import base64;
 
-url = "https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7";
+v2ray_url_github = "https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7";
+
+ss_url_github = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7";
 
 #url = "https://gitlab.com/zhifan999/fq/-/wikis/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
 
@@ -10,7 +12,9 @@ url = "https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A
 
 #url = "https://www.baidu.com"
 
-html = requests.get(url).text;
+v2ray_html = requests.get(v2ray_url_github).text;
+
+ss_html = requests.get(ss_url_github).text;
 
 #print(html)
 
@@ -28,36 +32,29 @@ def read_from_local(file_name):
 #html = read_from_local("v2ray_demo.html");
 
 #github
-urls = re.findall(r"data-snippet-clipboard-copy-content=\"([^\"]+)\"", html);
+v2ray_urls = re.findall(r"data-snippet-clipboard-copy-content=\"([^\"]+)\"", v2ray_html);
 
 #gitlab
-#urls = re.findall(r"```bash\\r\\n([^`]+)\\r\\n```", html);
+#v2ray_urls = re.findall(r"```bash\\r\\n([^`]+)\\r\\n```", html);
 
-subUrls = [];
-for url in urls:
+sub_urls = [];
+for url in v2ray_urls:
     if not re.match(r"vless|vmess|hysteria", url):
         url = f"vmess://{url}"
     elif not re.match("&amp;", url):
         url = url.replace("&amp;", "&");
+    sub_urls.append(url)
 
-    subUrls.append(url)
+ss_ssr_urls = re.findall(r"data-snippet-clipboard-copy-content=\"([^\"]+)\"", ss_html);
+ss_urls = filter ( lambda url : re.match("ss://", url) ,  ss_ssr_urls )
 
+print(f"{len(v2ray_urls)} v2ray_url, {len(ss_urls)} ss_urls")
 
 #format
-#subUrls = [u.replace("&amp;", "&")  for u in urls ]
+#sub_urls = [u.replace("&amp;", "&")  for u in urls ]
+sub_urls.extend(ss_urls);
+#print(sub_urls)
 
-#print(f"find {len(subUrls)} url")
-
-#print(subUrls)
-
-subContent = base64.b64encode("\n".join(subUrls).encode("utf-8")).decode("utf-8");
+subContent = base64.b64encode("\n".join(sub_urls).encode("utf-8")).decode("utf-8");
 write_to_local("v2ray_sub", subContent)
 print("succeed")
-#patten = r"vless://([a-f0-9-]+)@([\d.]+):(\d+)\?encryption=([^&]+)&security=([^&]+)&sni=([^&]+)&fp=([^&]+)&pbk=([^&]+)&sid=([^&]+)&spx=([^&]+)&type=([^&]+)";
-#patten = r"vless://([a-f0-9-]+)@([\d.]+):(\d+)";
-#patten = r"<div class=\"wiki-page-details\">\n.*\n.*\n.*"
-#divs = re.findall( patten, html)
-
-#write_to_local(divs[0])
-
-
